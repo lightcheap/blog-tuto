a8652aa045bae6a2b5959ae5a2adfac56eb1696b
光安雅義 <lightcheap358@gmail.com>
2022-05-03 01:03:49 +0900

    ブログ用のテーブル用マイグレファイル

---
+++ program/database/migrations/2022_05_01_122819_create_blogs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 190);
+            $table->longText('content');
+            $table->string('img_title', 190);
+            $table->string('img_path', 190);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
