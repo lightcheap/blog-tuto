9d578200854d0493dc9229857c6b84d85e420f03
光安雅義 <lightcheap358@gmail.com>
2022-05-03 01:00:51 +0900

    フォームを表示してみる。仮作成

--- program/resources/views/blog/form.blade.php
+++ program/resources/views/blog/form.blade.php
@@ -1,1 +1,15 @@
 <h1>ブログのフォームページ</h1>
+
+<form id="form" method="POST" enctype="multipart/form-data">
+    @csrf
+    <div class="form-group">
+      <label class="form-label">記事</label>
+      <meta name="csrf-token" content="{{ csrf_token() }}">
+      <div id="editor"></div>
+      <input id="content" type="hidden" name="content"
+        value="{{ $article->content ?? ""}}">
+    </div>
+    <div class="form-group">
+      <button id="submit-btn" class="btn btn-primary" type="button">登録する</button>
+    </div>
+  </form>
